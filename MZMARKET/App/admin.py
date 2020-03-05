1fd90cb5133420120261a0ee8741566225e2cf15
ccc179 <ccc179@gmail.com>
2020-03-05 23:14:40 +0800

    完善了所有页面的点击事件1.2

--- MZMARKET/App/admin.py
+++ MZMARKET/App/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 from App.models import MainMustbuy, MainNav, MainShop,MainShow, MainWheel, FoodType, Goods, MZUser, Cart, ServerInfo
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = 'u_name', 'u_password'
+    search_fields = 'u_name'
+
+
 '''账号tom 密码：门 '''
 # 注册Model类
 admin.site.register(MainShow)
@@ -10,6 +17,6 @@ admin.site.register(MainMustbuy)
 admin.site.register(MainShop)
 admin.site.register(FoodType)
 admin.site.register(Goods)
-admin.site.register(MZUser)
+admin.site.register(MZUser, UserAdmin)
 admin.site.register(Cart)
 admin.site.register(ServerInfo)
